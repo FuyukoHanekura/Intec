c7883d94c66584befef514fccd4a19854dfe490f
FuyukoHanekura <bevissurprise.thomas@gmail.com>
2022-03-28 15:11:49 +0200

    A lot of thing

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="/assets/img/#.png" alt="Logo">
+        </div>
+        <div class="searchbar">
+            <form action="">
+                <input type="search" name="" id="">
+                <input type="submit" value="">
+            </form>
+        </div>
+        <div>
+            <a href="#">Compte</a>
+            <a href="#">Panier</a>
+        </div>
+    </header>
+    <main>
+        <div class="banner">
+            <img src="assets/img/#.png" alt="Banner">
+        </div>
+        <div class="wrapper">
+            <div class="computers">
+                <h2 class="categories-title">Computers</h2>
+                <div class="subcategories">
+                    <ul class="subc1">
+                        <li>
+                            <div class="items">
+                                <img src="assets/img/#.png" alt="">
+                                <a href="#" class="title"></a>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="items">
+                                <img src="" alt="">
+                                <a href="#" class="title"></a>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="items">
+                                <img src="" alt="">
+                                <a href="#" class="title"></a>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="items">
+                                <img src="" alt="">
+                                <a href="#" class="title"></a>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>    
+</body>
+</html>
