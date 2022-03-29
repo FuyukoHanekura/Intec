6f6c9818eeb91f3a8ba7cc9179390707c889b372
FuyukoHanekura <bevissurprise.thomas@gmail.com>
2022-03-29 10:55:42 +0200

    Few thing like trying to make a carousel

--- index.php
+++ index.php
@@ -6,17 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="/assets/img/Intec_logo.jpg" type="image/x-icon">
     <title>Intec - Exchange Hardware</title>
 </head>
 <body>
     <header>
         <div class="logo">
-            <img class="brandlogo" src="assets/img/logo.png" alt="Logo">
+            <img class="brandlogo" src="assets/img/Intec_logo.svg" alt="Logo">
         </div>
         <div class="searchbar">
             <form action="">
                 <input type="search" name="" id="">
-                <input type="submit" value="">
+                <button type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </button>
             </form>
         </div>
         <div>
@@ -26,10 +31,69 @@
     </header>
     <main>
         <div class="banner">
-            <img src="assets/img/#.png" alt="Banner">
+            
         </div>
         <div class="wrapper">
-            <div class="computers">
+            <div class="categories">
+                <h2 class="categories-title">Computers</h2>
+                <div class="subcategories">
+                    <ul class="subc1">
+                        <li>
+                            <div class="items">
+                                <img src="assets/img/#.png" alt="">
+                                <a href="#" class="title">Laptops</a>
+                                <ul>
+                                    <li>Laptop</li>
+                                    <li>MacBook</li>
+                                    <li>Bag, Backpack, Case</li>
+                                    <li>2.5" Hard Drive</li>
+                                    <li>Laptop accessories</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="items">
+                                <img src="" alt="">
+                                <a href="#" class="title">Components</a>
+                                <ul>
+                                    <li>Mother Board</li>
+                                    <li>Graphics cards</li>
+                                    <li>Processor</li>
+                                    <li>Storage</li>
+                                    <li>Powers Units</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="items">
+                                <img src="" alt="">
+                                <a href="#" class="title">Desktop Computers</a>
+                                <ul>
+                                    <li>PC</li>
+                                    <li>Mac Computer</li>
+                                    <li>NAS Server</li>
+                                    <li>Barebone PC</li>
+                                    <li>Server</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="items">
+                                <img src="" alt="">
+                                <a href="#" class="title">PC Peripherals</a>
+                                <ul>
+                                    <li>Keyboard</li>
+                                    <li>Mice</li>
+                                    <li>Monitor</li>
+                                    <li>External storage</li>
+                                    <li>Printer</li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="categories">
                 <h2 class="categories-title">Computers</h2>
                 <div class="subcategories">
                     <ul class="subc1">
