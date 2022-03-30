35a38bf0cbe053810940ae107c5b32fc9b2213d7
FuyukoHanekura <bevissurprise.thomas@gmail.com>
2022-03-30 11:33:32 +0200

    Adding a lot of file

---
+++ inc/header.inc.php
@@ -0,0 +1,17 @@
+<header>
+        <div class="logo">
+            <img src="assets/img/Intec_logo.svg" alt="">
+        </div>
+        <div class="searchbar">
+            <form action="">
+                <input type="search" name="" id="" placeholder="Type to search">
+                <a href="" class="search-btn">
+                    <span class="material-icons">search</span>
+                </a>
+            </form>
+        </div>
+        <div class="acc-cart">
+            <a href="#"><span class="material-icons">person</span></a>
+            <a href="#"><span class="material-icons">shopping_cart</span></a>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -5,30 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+    <!-- Fonts -->
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="/assets/img/Intec_logo.jpg" type="image/x-icon">
     <title>Intec - Exchange Hardware</title>
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img class="brandlogo" src="assets/img/Intec_logo.svg" alt="Logo">
-        </div>
-        <div class="searchbar">
-            <form action="">
-                <input type="search" name="" id="">
-                <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg>
-                </button>
-            </form>
-        </div>
-        <div>
-            <a href="#">Account</a>
-            <a href="#">Carts</a>
-        </div>
-    </header>
+    <?php 
+        include ("inc/header.inc.php");
+    ?>
     <main>
         <div class="banner">
             
@@ -94,58 +80,58 @@
                 </div>
             </div>
             <div class="categories">
-                <h2 class="categories-title">Computers</h2>
+                <h2 class="categories-title">Other Categories</h2>
                 <div class="subcategories">
                     <ul class="subc1">
                         <li>
                             <div class="items">
                                 <img src="assets/img/#.png" alt="">
-                                <a href="#" class="title">Laptops</a>
+                                <a href="#" class="title">Mobiles</a>
                                 <ul>
-                                    <li>Laptop</li>
-                                    <li>MacBook</li>
-                                    <li>Bag, Backpack, Case</li>
-                                    <li>2.5" Hard Drive</li>
-                                    <li>Laptop accessories</li>
+                                    <li>Smarthphones</li>
+                                    <li>Mobiles Phones</li>
+                                    <li>Phones cases</li>
+                                    <li>Phones charger</li>
+                                    <li>Memory Card</li>
                                 </ul>
                             </div>
                         </li>
                         <li>
                             <div class="items">
                                 <img src="" alt="">
-                                <a href="#" class="title">Components</a>
+                                <a href="#" class="title">Office</a>
                                 <ul>
-                                    <li>Mother Board</li>
-                                    <li>Graphics cards</li>
-                                    <li>Processor</li>
-                                    <li>Storage</li>
-                                    <li>Powers Units</li>
+                                    <li>Printer cartridge</li>
+                                    <li>Printer paper</li>
+                                    <li>Calculator</li>
+                                    <li>Paper Shredder</li>
+                                    <li>Binding machine</li>
                                 </ul>
                             </div>
                         </li>
                         <li>
                             <div class="items">
                                 <img src="" alt="">
-                                <a href="#" class="title">Desktop Computers</a>
+                                <a href="#" class="title">Camera</a>
                                 <ul>
-                                    <li>PC</li>
-                                    <li>Mac Computer</li>
-                                    <li>NAS Server</li>
-                                    <li>Barebone PC</li>
-                                    <li>Server</li>
+                                    <li>Compact Camera</li>
+                                    <li>Memory card</li>
+                                    <li>Camera Tripod</li>
+                                    <li>Camera lens</li>
+                                    <li>Camera Bag</li>
                                 </ul>
                             </div>
                         </li>
                         <li>
                             <div class="items">
                                 <img src="" alt="">
-                                <a href="#" class="title">PC Peripherals</a>
+                                <a href="#" class="title">Furniturs</a>
                                 <ul>
-                                    <li>Keyboard</li>
-                                    <li>Mice</li>
-                                    <li>Monitor</li>
-                                    <li>External storage</li>
-                                    <li>Printer</li>
+                                    <li>Office chair</li>
+                                    <li>Desks</li>
+                                    <li>Air treatments</li>
+                                    <li>Desk lamp</li>
+                                    <li>Office furniture</li>
                                 </ul>
                             </div>
                         </li>
@@ -154,8 +140,14 @@
             </div>
         </div>
     </main>
-    <footer>
-
+    <footer class="">
+        <span class="text-muted">Tout droits réservés </span>
+        <div class="link">
+            <p class="section" >About us</p>
+            <p class="section" >FAQ</p>
+            <p class="section" >Legal information</p>
+            <p class="section" >Privacy policy</p>
+        </div>
     </footer>    
 </body>
 </html>
