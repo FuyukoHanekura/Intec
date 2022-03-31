1e71757be74d50778f841622ea3608b06607e11a
FuyukoHanekura <bevissurprise.thomas@gmail.com>
2022-03-31 12:33:37 +0200

    Doing homepage

--- index.php
+++ index.php
@@ -23,125 +23,105 @@
             <div class="categories">
                 <h2 class="categories-title">Computers</h2>
                 <div class="subcategories">
-                    <ul class="subc1">
-                        <li>
-                            <div class="items">
-                                <img src="assets/img/#.png" alt="">
-                                <a href="#" class="title">Laptops</a>
-                                <ul>
-                                    <li>Laptop</li>
-                                    <li>MacBook</li>
-                                    <li>Bag, Backpack, Case</li>
-                                    <li>2.5" Hard Drive</li>
-                                    <li>Laptop accessories</li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="items">
-                                <img src="" alt="">
-                                <a href="#" class="title">Components</a>
-                                <ul>
-                                    <li>Mother Board</li>
-                                    <li>Graphics cards</li>
-                                    <li>Processor</li>
-                                    <li>Storage</li>
-                                    <li>Powers Units</li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="items">
-                                <img src="" alt="">
-                                <a href="#" class="title">Desktop Computers</a>
-                                <ul>
-                                    <li>PC</li>
-                                    <li>Mac Computer</li>
-                                    <li>NAS Server</li>
-                                    <li>Barebone PC</li>
-                                    <li>Server</li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="items">
-                                <img src="" alt="">
-                                <a href="#" class="title">PC Peripherals</a>
-                                <ul>
-                                    <li>Keyboard</li>
-                                    <li>Mice</li>
-                                    <li>Monitor</li>
-                                    <li>External storage</li>
-                                    <li>Printer</li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
+                    <div class="items it1">
+                        <img src="assets/img/laptop.webp" alt=""><br/>
+                        <a href="#" class="title">Laptops</a>
+                        <ul>
+                            <li>Laptop</li>
+                            <li>MacBook</li>
+                            <li>Bag, Backpack, Case</li>
+                            <li>2.5" Hard Drive</li>
+                            <li>Laptop accessories</li>
+                        </ul>
+                    </div>
+                    <div class="items it2">
+                        <img src="assets/img/graphiccard.png" alt=""><br/>
+                        <a href="#" class="title">Components</a>
+                        <ul>
+                            <li>Mother Board</li>
+                            <li>Graphics cards</li>
+                            <li>Processor</li>
+                            <li>Storage</li>
+                            <li>Powers Units</li>
+                        </ul>
+                    </div>
+                    <div class="items it3">
+                        <img src="assets/img/desktop.png" alt=""><br/>
+                        <a href="#" class="title">Desktop Computers</a>
+                        <ul>
+                            <li>PC</li>
+                            <li>Mac Computer</li>
+                            <li>NAS Server</li>
+                            <li>Barebone PC</li>
+                            <li>Server</li>
+                        </ul>
+                    </div>
+                    <div class="items it4">
+                        <img src="assets/img/Kb.png" alt=""><br/>
+                        <a href="#" class="title">PC Peripherals</a>
+                        <ul>
+                            <li>Keyboard</li>
+                            <li>Mice</li>
+                            <li>Monitor</li>
+                            <li>External storage</li>
+                            <li>Printer</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="categories">
                 <h2 class="categories-title">Other Categories</h2>
                 <div class="subcategories">
-                    <ul class="subc1">
-                        <li>
-                            <div class="items">
-                                <img src="assets/img/#.png" alt="">
-                                <a href="#" class="title">Mobiles</a>
-                                <ul>
-                                    <li>Smarthphones</li>
-                                    <li>Mobiles Phones</li>
-                                    <li>Phones cases</li>
-                                    <li>Phones charger</li>
-                                    <li>Memory Card</li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="items">
-                                <img src="" alt="">
-                                <a href="#" class="title">Office</a>
-                                <ul>
-                                    <li>Printer cartridge</li>
-                                    <li>Printer paper</li>
-                                    <li>Calculator</li>
-                                    <li>Paper Shredder</li>
-                                    <li>Binding machine</li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="items">
-                                <img src="" alt="">
-                                <a href="#" class="title">Camera</a>
-                                <ul>
-                                    <li>Compact Camera</li>
-                                    <li>Memory card</li>
-                                    <li>Camera Tripod</li>
-                                    <li>Camera lens</li>
-                                    <li>Camera Bag</li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="items">
-                                <img src="" alt="">
-                                <a href="#" class="title">Furniturs</a>
-                                <ul>
-                                    <li>Office chair</li>
-                                    <li>Desks</li>
-                                    <li>Air treatments</li>
-                                    <li>Desk lamp</li>
-                                    <li>Office furniture</li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
+                    <div class="items it1">
+                        <img src="assets/img/phone.png" alt=""><br/>
+                        <a href="#" class="title">Mobiles</a>
+                        <ul>
+                            <li>Smarthphones</li>
+                            <li>Mobiles Phones</li>
+                            <li>Phones cases</li>
+                            <li>Phones charger</li>
+                            <li>Memory Card</li>
+                        </ul>
+                    </div>
+                    <div class="items it2">
+                        <img src="assets/img/printer.png" alt=""><br/>
+                        <a href="#" class="title">Office</a>
+                        <ul>
+                            <li>Printer cartridge</li>
+                            <li>Printer paper</li>
+                            <li>Calculator</li>
+                            <li>Paper Shredder</li>
+                            <li>Binding machine</li>
+                        </ul>
+                    </div>
+                    <div class="items it3">
+                        <img src="assets/img/camera.png" alt=""><br/>
+                        <a href="#" class="title">Camera</a>
+                        <ul>
+                            <li>Compact Camera</li>
+                            <li>Memory card</li>
+                            <li>Camera Tripod</li>
+                            <li>Camera lens</li>
+                            <li>Camera Bag</li>
+                        </ul>
+                    </div>
+                    <div class="items it4">
+                        <img src="assets/img/desk.png" alt=""><br/>
+                        <a href="#" class="title">Furniturs</a>
+                        <ul>
+                            <li>Office chair</li>
+                            <li>Desks</li>
+                            <li>Air treatments</li>
+                            <li>Desk lamp</li>
+                            <li>Office furniture</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </main>
     <footer class="">
-        <span class="text-muted">Tout droits réservés </span>
+        <span class="text-muted">All rights reserved </span>
         <div class="link">
             <p class="section" >About us</p>
             <p class="section" >FAQ</p>
